8817483616bdc6b32f1021c348a906de9df4fdd7
Mehmet Sezer <m.szr98@gmail.com>
2021-04-16 21:43:44 +0300

    Initial commit

---
+++ backend/bbm486-group10/src/main/java/com/bbm486group10/Bbm486Group10Application.java
@@ -0,0 +1,13 @@
+package com.bbm486group10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbm486Group10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbm486Group10Application.class, args);
+	}
+
+}
---
+++ backend/bbm486-group10/src/test/java/com/bbm486group10/Bbm486Group10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbm486group10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbm486Group10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
