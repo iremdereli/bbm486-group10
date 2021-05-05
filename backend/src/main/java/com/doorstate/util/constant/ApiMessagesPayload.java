428fcfd03fb1c63bcd5f20ffc9925d86dc6a5d5d
Mehmet Sezer <m.szr98@gmail.com>
2021-05-05 22:26:52 +0300

    Add getter setter to payload

--- backend/src/main/java/com/doorstate/util/constant/ApiMessagesPayload.java
+++ backend/src/main/java/com/doorstate/util/constant/ApiMessagesPayload.java
@@ -6,4 +6,12 @@ public class ApiMessagesPayload {
   public ApiMessagesPayload(String message) {
     this.message = message;
   }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
