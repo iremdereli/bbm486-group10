304a4c4583e3f80ea8c1925f07f3554a2a2d1894
guney <kirikguney@gmail.com>
2021-05-06 00:30:52 +0300

    messaged updated

--- backend/src/main/java/com/doorstate/util/constant/ApiMessages.java
+++ backend/src/main/java/com/doorstate/util/constant/ApiMessages.java
@@ -1,14 +1,14 @@
 package com.doorstate.util.constant;
 
 public class ApiMessages {
-  public static final String ALREADY_CLOSED_EXCEPTION = "Already Closed";
-  public static final String ALREADY_LOCKED_EXCEPTION = "Already Locked";
-  public static final String ALREADY_OPEN_EXCEPTION = "Already Opened";
-  public static final String CLOSED_DOOR_CAN_NOT_BE_UNLOCKED = "Closed door can't be unlocked";
-  public static final String LOCKED_DOOR_CAN_NOT_BE_OPENED = "Locked door can't be opened";
-  public static final String LOCKED_DOOR_CAN_NOT_BE_CLOSED = "Locked door can't be closed";
-  public static final String OPENED_DOOR_CAN_NOT_BE_LOCKED = "Opened door can't be locked";
-  public static final String OPENED_DOOR_CAN_NOT_BE_UNLOCKED = "Opened door can't be unlocked";
+  public static final String ALREADY_CLOSED_EXCEPTION = "Closed door can not be closed again.";
+  public static final String ALREADY_LOCKED_EXCEPTION = "Locked door can not be locked again.";
+  public static final String ALREADY_OPEN_EXCEPTION = "Open door can not be opened again.";
+  public static final String CLOSED_DOOR_CAN_NOT_BE_UNLOCKED = "Closed door can not be unlocked.";
+  public static final String LOCKED_DOOR_CAN_NOT_BE_OPENED = "Locked door can not be opened.";
+  public static final String LOCKED_DOOR_CAN_NOT_BE_CLOSED = "Locked door can not be closed.";
+  public static final String OPENED_DOOR_CAN_NOT_BE_LOCKED = "Open door can not be locked.";
+  public static final String OPENED_DOOR_CAN_NOT_BE_UNLOCKED = "Open door can not be unlocked.";
   public static final String SUCCESS_MESSAGE = "Success";
 
   public static ApiMessagesPayload createApiMessage(String message) {
