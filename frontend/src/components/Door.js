eb308b4b7bb92ee8384f02540865255d7caf93e0
Emre <kosenemre@gmail.com>
2021-05-06 00:17:11 +0300

    Add door

---
+++ frontend/src/components/Door.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Door = ({ doorState }) => {
+  return (
+    <div className="backDoor">
+      <div
+        className={doorState === "OpenDoorState" ? "door doorOpen" : "door"}
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        {doorState === "LockedDoorState" ? (
+          <i
+            className="fas fa-lock fa-lg"
+            style={{
+              color: "white",
+              marginRight: "15px",
+            }}
+          ></i>
+        ) : (
+          <i
+            className="fas fa-lock-open fa-2x"
+            style={{
+              color: "white",
+              marginRight: "15px",
+            }}
+          ></i>
+        )}
+      </div>
+    </div>
+  );
+};
