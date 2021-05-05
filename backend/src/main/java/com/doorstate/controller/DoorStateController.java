abf2e51923aa44d0f5cfca2f97c5d4f38484e0da
Mehmet Sezer <m.szr98@gmail.com>
2021-05-05 22:03:06 +0300

    Disable cross origin

--- backend/src/main/java/com/doorstate/controller/DoorStateController.java
+++ backend/src/main/java/com/doorstate/controller/DoorStateController.java
@@ -4,10 +4,12 @@ import com.doorstate.model.Door;
 import com.doorstate.model.state.ClosedDoorState;
 import com.doorstate.util.constant.ApiMessages;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class DoorStateController {
   public final Door door = new Door(new ClosedDoorState());
