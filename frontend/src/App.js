a46f484e59b4f79f152f3239051c1f03715a1d7b
Emre <kosenemre@gmail.com>
2021-05-05 22:28:08 +0300

    Update files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/state", {
+      method: "GET",
+    }).then(async (res) => {
+      console.log(res.status);
+      console.log(await res.json());
+    });
+  });
+
   return (
     <div className="App">
-      <p>Paternik Pizzasi</p>
+      <p>State: </p>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        Door
+      </div>
     </div>
   );
 }
